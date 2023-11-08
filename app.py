ea929eed32912c4733a8ebc165781d79e30540ab
SergioCabreraDev <scabrera.main@gmail.com>
2023-11-08 18:13:00 +0100

    nuevocommit

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     socketio.run(app, host='0.0.0.0', port=port)
 
 
